0e32dd394a507bf2a5de4d474281987dd0258e8a
Artur Harutyunyan <arturhar@outlook.com>
2025-05-22 20:42:22 +0400

    Fix

--- LibraryManagementSystem/MyPlayground.playground/Contents.swift
+++ LibraryManagementSystem/MyPlayground.playground/Contents.swift
@@ -53,14 +53,11 @@ class Library {
         items[book.id] = book
     }
     func borrowItem(by id: String) throws -> Item {
-        guard var borrowableItem = items[id] as? Borrowable else {
-            guard items[id] != nil else {
-                throw LibraryError.itemNotFound
-            }
-            throw LibraryError.itemNotBorrowable
-        }
         guard let item = items[id] else {
             throw LibraryError.itemNotFound
+        }
+        guard let borrowableItem = item as? Borrowable else {
+            throw LibraryError.itemNotBorrowable
         }
         if borrowableItem.isBorrowed {
             throw LibraryError.alreadyBorrowed
@@ -68,8 +65,26 @@ class Library {
         borrowableItem.borrowDate = Date()
         borrowableItem.returnDate = Calendar.current.date(byAdding: .day, value: 7, to: Date())
         borrowableItem.isBorrowed = true
-        
-        items[id] = borrowableItem as? Item
         return item
     }
 }
+
+
+//let library = Library()
+//let book1 = Book(id: "124", title: "Harry Potter and the Chamber of Secrets", author: "Kanye West")
+//
+//library.addBook(book1)
+//
+//do {
+//    try library.borrowItem(by: book1.id)
+//    print("Successfully borrowed the book \(book1.title)")
+//    try library.borrowItem(by: book1.id)
+//} catch LibraryError.alreadyBorrowed {
+//    print("Error: \(book1.title) is already borrowed")
+//} catch LibraryError.itemNotBorrowable {
+//    print("Error: \(book1.title) is not borrowable")
+//} catch LibraryError.itemNotFound {
+//    print("Error: \(book1.title) was not found")
+//} catch {
+//    print("Error: Uncaught exception")
+//}
