d9229ce84642e23e96536273fe1e38dc80e19bd6
Artur Harutyunyan <arturhar@outlook.com>
2025-07-24 15:17:43 +0400

    Fix

--- Foundation/Part 4/Task 4/Task 4.playground/Contents.swift
+++ Foundation/Part 4/Task 4/Task 4.playground/Contents.swift
@@ -12,16 +12,16 @@ func executeTasks() {
     let queueFinal = DispatchQueue(label: "com.example.finalQueue")
     let group = DispatchGroup()
     
-    group.enter()
     queue1.async {
+        group.enter()
+        defer { group.leave() }
         executeTask(1, delay: 2)
-        group.leave()
     }
     
-    group.enter()
     queue2.async {
+        group.enter()
+        defer { group.leave() }
         executeTask(2, delay: 3)
-        group.leave()
     }
     
     group.notify(queue: queueFinal) {
